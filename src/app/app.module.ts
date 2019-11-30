f86b78e0a17dda5e46f54b5b57434fe509d49a48
Javier O <javieropriego@hotmail.com>
2019-11-30 11:36:08 +0100

    Modificados los parametros de la barra lateral

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SupbarComponent } from './supbar/supbar.component';
 import { LeftbarComponent } from './leftbar/leftbar.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule }   from '@angular/forms';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { ParametrosmenuComponent } from './parametrosmenu/parametrosmenu.component';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule} from '@angular/material/divider';
 
 
 @NgModule({
@@ -30,7 +31,10 @@ import {MatButtonModule} from '@angular/material/button';
     FormsModule,
     MatCheckboxModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatDividerModule
   ],
   exports: [
     MatInputModule
