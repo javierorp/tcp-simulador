b4848e41e617ec48539ee2dbf976225cc85b7c39
Javier O <javierorp@outlook.com>
2020-08-20 15:40:22 +0200

    Adaptación a móviles y ampliación de navegadores compatibles

--- src/app/supbar/supbar.component.ts
+++ src/app/supbar/supbar.component.ts
@@ -19,15 +19,11 @@ export class SupbarComponent {
   idiomas: string[] = ["Español", "English"];
   idiomaSeleccionado: string = "Español";
   bandera: string = "spain";
-  movil: boolean = false;
   public idiomaActivo = 'es';
 
   constructor(private titleService: Title, private modalService: NgbModal, private translate: TranslateService) {
 
     var naviLang = navigator.language;
-
-    // Si se visualiza en un movil no aparece el titulo de la pagina
-    this.movil = window.screen.width <= 705 ? true : false;
     
     // Idioma por defecto en el navegador
     if (naviLang.toUpperCase().indexOf("ES") == 0) { // Si el navegador se encuentra en espanyol se selecciona este idioma por defecto
